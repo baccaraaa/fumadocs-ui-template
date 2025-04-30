163482772a147644b8ebba0dfe3ecfe1a97df35e
baccaraaa <baccaraaa@rambler.ru>
2025-04-30 17:53:58 +0300

    fixes last

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,7 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import { redirect } from 'next/navigation';
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+export default function Layout() {
+  redirect('/docs');
+  
+  return null;
 }
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { BookIcon, LayoutDashboard, Send, UsersRound } from 'lucide-react';
 
 /**
  * Shared layout configurations
@@ -19,14 +20,33 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        20rank docs
       </>
     ),
   },
   links: [
     {
+      icon: <BookIcon />,
       text: 'Documentation',
       url: '/docs',
+      active: 'nested-url',
+    },
+    {
+      icon: <LayoutDashboard />,
+      text: 'Dashboard',
+      url: 'https://20rank.ru/dashboard',
+      active: 'nested-url',
+    },
+    {
+      icon: <Send />,
+      text: 'Telegram Community',
+      url: 'https://t.me/twentyrank',
+      active: 'nested-url',
+    },
+    {
+      icon: <UsersRound />,
+      text: 'Discord Channel',
+      url: 'https://20rank.ru/dashboard',
       active: 'nested-url',
     },
   ],
